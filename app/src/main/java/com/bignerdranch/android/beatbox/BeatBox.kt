d3e2b30f4827390939227c9077b0fc548aded6a5
hyuna <hyuna9787@gmail.com>
2021-07-03 01:29:58 +0900

    Apply chapter19
    - Add binding code for MVVM

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -8,13 +8,28 @@ private const val SOUNDS_FOLDER = "sample_sounds"
 
 class BeatBox(private val assets: AssetManager) {
 
-    fun loadSounds(): List<String> {
+    val sounds: List<Sound>
+
+    init {
+        sounds = loadSounds()
+    }
+
+    private fun loadSounds(): List<Sound> {
+
+        val soundNames: Array<String>
+
         try {
-            val soundNames = assets.list(SOUNDS_FOLDER)!!
-            return soundNames.asList()
+            soundNames = assets.list(SOUNDS_FOLDER)!!
         } catch (e: Exception) {
             return emptyList()
         }
-    }
 
+        val sounds = mutableListOf<Sound>()
+        soundNames.forEach { fileName ->
+            val assetPath = "$SOUNDS_FOLDER/$fileName"
+            val sound = Sound(assetPath)
+            sounds.add(sound)
+        }
+        return sounds
+    }
 }
--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -17,23 +17,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         beatBox = BeatBox(assets)
-        beatBox.loadSounds()
 
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
-            adapter = SoundAdapter()
+            adapter = SoundAdapter(beatBox.sounds)
         }
     }
 
     private inner class SoundHolder(private val binding: ListItemSoundBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            binding.viewModel = SoundViewModel()
+        }
+
+        fun bind(sound: Sound) {
+            binding.apply {
+                viewModel.sound = sound
+                // 레이아웃을 즉각적으로 변경하기 위함
+                executePendingBindings()
+            }
+        }
     }
 
-    private inner class SoundAdapter() : RecyclerView.Adapter<SoundHolder>() {
+    private inner class SoundAdapter(private val sounds: List<Sound>) :
+        RecyclerView.Adapter<SoundHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
             val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
                 layoutInflater,
@@ -45,9 +56,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            val sound = sounds[position]
+            holder.bind(sound)
         }
 
-        override fun getItemCount(): Int = 0
+        override fun getItemCount(): Int = sounds.size
 
     }
 }
