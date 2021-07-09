ed85541ea4870b19ddfdd3bfb7598f48a7fc66ce
hyuna <hyuna9787@gmail.com>
2021-07-09 23:55:39 +0900

    Add unit test code

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -1,12 +1,29 @@
 package com.bignerdranch.android.beatbox
 
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.Is.`is`
 import org.junit.Before
-
-import org.junit.Assert.*
+import org.junit.Test
 
 class SoundViewModelTest {
 
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
     @Before
     fun setUp() {
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound = sound
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle() {
+        MatcherAssert.assertThat(subject.title, `is`(sound.name))
+    }
+
+    @Test
+    fun callsBeatBoxPlayOnButtonClicked() {
+        subject.onButtonClicked()
     }
 }
